43688a7a76cc5447bc91b07391b5cb188019785f
Johnny <stagiaire-technique@glotelhomail.com>
2022-07-30 14:24:52 +0200

    init

--- database/migrations/2022_06_07_093548_create_votes_table.php
+++ database/migrations/2022_06_07_093548_create_votes_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('reponse_id')->constrained();
             $table->foreignId('user_id')->constrained();
-            $table->string('vote');
+            $table->bigInteger('vote');
             $table->timestamps();
         });
     }
