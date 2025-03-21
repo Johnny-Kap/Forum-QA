0ecf0d0b9101409a9d91d603b67a267b78eed3ad
Johnny-Kap <johnnyfokou@gmail.com>
2025-03-21 11:33:31 +0100

    Update

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -84,8 +84,9 @@
                         </div>
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item dropdown">
+                             
                                 <a id="navbarDropdown" style="color:black;" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                     {{ Str::limit(Auth::user()->name, 10, '...') }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
